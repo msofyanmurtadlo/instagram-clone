f448d0fe8ec5258441fa2c85365af57de5856138
msofyanmurtadlo <sofyanmurtadlo6@gmail.com>
2025-02-11 20:28:19 +0700

    design: navbar & sidebar

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,6 +3,102 @@ import { ref } from "vue";
 import { Link } from "@inertiajs/vue3";
 
 import Magnify from "vue-material-design-icons/Magnify.vue";
+import HeartOutline from "vue-material-design-icons/HeartOutline.vue";
+import HomeOutline from "vue-material-design-icons/HomeOutline.vue";
+import Compass from "vue-material-design-icons/Compass.vue";
+import SendOutline from "vue-material-design-icons/SendOutline.vue";
+import Plus from "vue-material-design-icons/Plus.vue";
+import AccountOutline from "vue-material-design-icons/AccountOutline.vue";
+import ChevronLeft from "vue-material-design-icons/ChevronLeft.vue";
+import AccountPlusOutline from "vue-material-design-icons/AccountPlusOutline.vue";
+import MenuItem from "@/Components/MenuItem.vue";
 </script>
 
-<template></template>
+<template>
+    <div id="MainLayout" class="w-full h-screen">
+        <div
+            id="TopNavHome"
+            class="fixed z-30 md:hidden block w-full bg-white h-[61px] border-b border-b-gray-300"
+            v-if="$page.url === '/'"
+        >
+            <div class="flex items-center justify-between h-full">
+                <Link href="/">
+                    <img
+                        class="w-[105px] ml-6 cursor-pointer"
+                        src="/insta-logo.png"
+                    />
+                </Link>
+                <div class="flex items-center w-[50%]">
+                    <div
+                        class="flex items-center w-full bg-gray-100 rounded-lg"
+                    >
+                        <Magnify class="pl-4" fillColor="#8e8e8e" :size="27" />
+                        <input
+                            type="text"
+                            placeholder="Search"
+                            class="bg-transparent w-full placeholder:[#000000] border-0 ring-0 focus:ring-0"
+                        />
+                    </div>
+                    <HeartOutline
+                        class="pl-4 pr-3"
+                        fillColor="#8e8e8e"
+                        :size="27"
+                    />
+                </div>
+            </div>
+        </div>
+        <div
+            v-if="$page.url !== '/'"
+            id="TopNavUser"
+            class="md:hidden fixed flex items-center justify-between z-30 w-full bg-white h-[61px] border-b border-b-gray-300"
+        >
+            <Link href="/" class="px-4">
+                <ChevronLeft fillColor="#8e8e8e" :size="27" />
+            </Link>
+            <div class="font-extrabold text-lg">Name Here</div>
+            <AccountPlusOutline :size="30" class="cursor-pointer px-4" />
+        </div>
+        <div
+            id="SIdeNav"
+            class="fixed h-full bg-white xl:w-[280px] w-[80px] md:block hidden border-r border-r-gray-300"
+        >
+            <Link href="/">
+                <img
+                    class="xl:hidden block w-[25px] mt-10 ml-[25px] mb-10 cursor-pointer"
+                    src="/insta-logo-small.png"
+                />
+                <img
+                    class="xl:block hidden w-[120px] mt-10 ml-6 mb-10 cursor-pointer"
+                    src="/insta-logo.png"
+                />
+            </Link>
+            <div class="px-3">
+                <Link href="/">
+                    <MenuItem iconString="Home" class="mb-4" />
+                </Link>
+                <MenuItem iconString="Search" class="mb-4" />
+                <MenuItem iconString="Explore" class="mb-4" />
+                <MenuItem iconString="Messages" class="mb-4" />
+                <MenuItem iconString="Notifications" class="mb-4" />
+                <MenuItem iconString="Profile" class="mb-4" />
+            </div>
+            <Link href="/" class="absolute bottom-0 px-3 w-full">
+                <MenuItem iconString="Log out" class="mb-4" />
+            </Link>
+        </div>
+        <div
+            class="flex lg:justify-between bg-white h-full w-[100%-280px] xl:pl-[280px] lg:pl-[100px] overflow-auto"
+        >
+            <div
+                class="mx-auto md:pt-6 pt-20"
+                :class="
+                    $page.url === '/' ? 'lg:w-8/12 w-full' : 'max-w-[1200px]'
+                "
+            >
+                <main>
+                    <slot />
+                </main>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,8 +1,10 @@
 <script setup>
-
+import MainLayout from "@/Layouts/MainLayout.vue";
 </script>
 
 <template>
-   <Head title="Welcome" />
-   <div class="bg-green-500">Home.vue</div>
+    <Head title="Welcome" />
+    <MainLayout>
+        <div class="bg-green-500">Home.vue</div>
+    </MainLayout>
 </template>
